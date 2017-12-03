624fe349495426a7416688b3c63e39a051316614
MasseyIsaako <lasiisaako@gmail.com>
2017-12-04 01:05:19 +1300

    finished frontpage
    added donate form, may need to add validation via JS. Added footer menu and link to portfolio. Responsive from Mobile to desktop.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
 (function($){
-	
 
-	var person = $(".navbar-collapse");
-	console.log(person);
+	// Smooth scroll to divs, this code only selects links with @href starting with '#'
+	$(document).on('click', 'a[href^="#"]', function(e) {
+	    // target element id
+	    var id = $(this).attr('href');
 
+	    // target element
+	    var $id = $(id);
+	    if ($id.length === 0) {
+	        return;
+	    }
+
+	    // prevent standard hash navigation (avoid blinking in IE)
+	    e.preventDefault();
+
+	    // top position relative to the document
+	    var pos = $id.offset().top - 102.5;
+
+	    // animated top scrolling
+	    $('body, html').animate({scrollTop: pos});
+	});
 
 }(jQuery));
