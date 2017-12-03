d100773ca38cb6f0d3875518facea99ba4f8beb1
MasseyIsaako <lasiisaako@gmail.com>
2017-12-04 01:53:47 +1300

    rearranged js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,26 @@
 (function($){
 
-	// Smooth scroll to divs, this code only selects links with @href starting with '#'
-	$(document).on('click', 'a[href^="#"]', function(e) {
-	    // target element id
-	    var id = $(this).attr('href');
+	$(document).ready(function(){
+		// Smooth scroll to divs, this code only selects links with @href starting with '#'
+		$(document).on('click', 'a[href^="#"]', function(e) {
+		    // target element id
+		    var id = $(this).attr('href');
 
-	    // target element
-	    var $id = $(id);
-	    if ($id.length === 0) {
-	        return;
-	    }
+		    // target element
+		    var $id = $(id);
+		    if ($id.length === 0) {
+		        return;
+		    }
 
-	    // prevent standard hash navigation (avoid blinking in IE)
-	    e.preventDefault();
+		    // prevent standard hash navigation (avoid blinking in IE)
+		    e.preventDefault();
 
-	    // top position relative to the document
-	    var pos = $id.offset().top - 102.5;
+		    // top position relative to the document
+		    var pos = $id.offset().top - 102.5;
 
-	    // animated top scrolling
-	    $('body, html').animate({scrollTop: pos});
+		    // animated top scrolling
+		    $('body, html').animate({scrollTop: pos});
+		});
 	});
 
 }(jQuery));
